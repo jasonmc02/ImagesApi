78e95acc8d13cd76c464de04b3264274961d2ee5
jason <jasonmc02@gmail.com>
2014-10-12 10:53:49 -0600

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ImagesApi::Application.config.secret_key_base = '59d1120394e352df3c64e24cb708a0002258e4e6f8e1e34d22e146a9afc1f4838091c5ff85ad32da8eeaf3b45627bf25005982ee12d8d803065600caf3db7b3f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ImagesApi::Application.config.session_store :cookie_store, key: '_ImagesApi_session'
